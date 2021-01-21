cb1fd6379a8c3f61081209488a255b7201e96070
fethiguc <77700661+fethiguc@users.noreply.github.com>
2021-01-21 17:21:59 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryDemo
+{
+    class MyDictionary<T1, T2>
+    {
+        public T1[] array1;
+        public T2[] array2;
+        public MyDictionary()
+        {
+            array1 = new T1[0];
+            array2 = new T2[0];
+        }
+        public void Add(T1 item1, T2 item2) 
+        {
+            T1[] tempArray1 = array1;
+            T2[] tempArray2 = array2;
+            array1 = new T1[array1.Length + 1];
+            array2 = new T2[array2.Length + 1];
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                array1[i] = tempArray1[i];
+                array2[i] = tempArray2[i]; 
+            }
+            array1[array1.Length - 1] = item1;
+            array2[array2.Length - 1] = item2;
+        }
+        public int Length
+        {
+            get { return array1.Length; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string>students=new MyDictionary<int,string>();
+            students.Add(1,"Ali");
+            students.Add(2, "Fethi");
+            students.Add(3, "Ayşe");
+            Console.WriteLine(students.Length);
+            Console.WriteLine(students.array1[0]);
+            Console.WriteLine(students.array2[0]);
+            Console.ReadKey();
+        }
+    }
+}
